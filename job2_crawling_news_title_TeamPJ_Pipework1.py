17f7e64792afa686b5fdcd8db57a531d1bb8a1da
User001 <12345@12345.com>
2023-10-11 15:35:38 +0900

    Test crawling, Crawled csv file in Politics.

---
+++ job2_crawling_news_title_TeamPJ_Pipework1.py
@@ -0,0 +1,65 @@
+# WebCrawlingNewsTitles in Politics
+# Produced by Pipework .
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
+import pandas as pd
+import re
+import time
+import datetime
+
+options = ChromeOptions()
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.60'
+options.add_argument('user-agent='+user_agent)
+options.add_argument('lang=ko_KR')
+# options.add_argument('window-size=1920x1080')
+# options.add_argument('disable-gpu')
+# options.add_argument('--no-sandbox')
+
+service = ChromeService(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=options)
+
+category = ['Politics']
+pages = [110]
+df_titles = pd.DataFrame()
+
+for l in range(len(category)):
+    titles = []
+    section_url = 'https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=10{}'.format(l)
+    for k in range(1, pages[l]+1):
+        url = section_url+'#&date=%2000:00:00&page={}'.format(k)
+        driver.get(url)
+        time.sleep(0.3)
+        for i in range(1, 6):
+            for j in range(1, 6):
+                try:
+                    title = driver.find_element('xpath','//*[@id="section_body"]/ul[{}]/li[{}]/dl/dt[2]/a'.format(i+1, j+1)).text
+                    title = re.compile('[^가-힣]').sub(' ',title)
+                    titles.append(title)
+                except:
+                    print('error {} {} {} {}'.format(l, k, i, j))
+    if k % 10 ==0:
+        df_section_title = pd.DataFrame(titles, columns=['titles'])
+        df_section_title['category'] = category[l]
+        df_titles = pd.concat([df_titles, df_section_title], ignore_index=True)
+        df_titles.to_csv('D:/Workspace_Python_1/Workspace_PyCharm_1/Team1011_ForkClone_1/Crawling_data_Pipework_Politics1/crawling_data_{}_{}.csv'.format(l, k), index=False)
+
+
+    df_section_title = pd.DataFrame(titles,columns=['titles'])
+    df_section_title['category']=category[l]
+    df_titles = pd.concat([df_titles, df_section_title], ignore_index=True)
+
+print(df_titles.head(30))
+print(df_titles.info())
+print(df_titles['category'].value_counts())
+#df_titles.to_csv('./crawling_data/naver_news_{}.csv'.format(datetime.datetime.now().strftime('%Y%m%d')))
+
+
+# //*[@id="section_body"]/ul[1]/li[1]/dl/dt[2]/a
+# //*[@id="section_body"]/ul[2]/li[1]/dl/dt[2]/a
+# //*[@id="paging"]/a[1]
+# //*[@id="paging"]/a[2]
+# //*[@id="section_body"]/ul[1]/li[1]/dl/dt[2]/a
---
+++ test01.py
@@ -0,0 +1,4 @@
+# Time was slept before several months..
+# Master Degree has dropped his sixteen-year-old dog when he arrived at his home.
+# I want to go to the music shop before my hair becoming Black.
+# I want to join to slow side of the shelf.
